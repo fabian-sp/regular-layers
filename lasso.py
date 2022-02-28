fix scaling factor 2

lasso.py
@@ -22,30 +22,33 @@ class LassoDataset(Dataset):
         return x, y
     
 
-def TorchLasso(X: torch.tensor, y: torch.tensor, l1: float, opt: torch.optim.Optimizer = None, bias: bool = True,\
-               verbose: bool = False, n_epochs: int = 2, lr:float = 0.01, batch_size: int = 1, store_iterates: bool = False):
+def TorchLasso(X: torch.tensor, y: torch.tensor, l1: float, opt: torch.optim.Optimizer=None, bias: bool=True,\
+               verbose: bool=False, n_epochs: int=2, lr:float=0.01, batch_size: int=1, store_iterates: bool=False):
     """
-
+    For :math:`X\\in\\mathbb{R}^{N\\times p}`, solves (if bias=True)
+    
+        min_{w,w0}  1/(2*N) * ||Xw + w0 -y||^2 + l1*||w||_1
+        
     Parameters
     ----------
     X : torch.tensor
-        DESCRIPTION.
+        Coefficient matrix.
     y : torch.tensor
-        DESCRIPTION.
+        Targets.
     l1 : float
-        DESCRIPTION.
+        Regularization parameter.
     opt : torch.optim.Optimizer, optional
         Optimizer. The default is SGD.
     bias : bool, optional
-        DESCRIPTION. The default is True.
+        Allow bias for layer. The default is True.
     verbose : bool, optional
-        DESCRIPTION. The default is False.
+        Verbosity. The default is False.
     n_epochs : int, optional
-        DESCRIPTION. The default is 2.
+        Number of epochs. The default is 2.
     lr : float, optional
         learning rate. The default is 1e-2
     batch_size : int, optional
-        DESCRIPTION. The default is 1.
+        batch size. The default is 1.
     store_iterates : bool, optional
         Whether to store the iterates. The default is False.
 
@@ -61,12 +64,12 @@ def TorchLasso(X: torch.tensor, y: torch.tensor, l1: float, opt: torch.optim.Opt
     N = X.shape[0]
     
     ds = LassoDataset(X, y)
-    dl = DataLoader(ds, batch_size = batch_size, shuffle = False)
-
-    model = L1Linear(l1=l1, in_features=p, out_features=1)
-    print(model.get_weight())
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=False)
     
-    opt = torch.optim.SGD(model.parameters(), lr = lr)
+    # loss has factor 1/N --> use 2*l1 for penalty
+    model = L1Linear(l1=2*l1, in_features=p, out_features=1)
+    
+    opt = torch.optim.SGD(model.parameters(), lr=lr)
         
     iterates = list()   
     info = {'train_loss':[], 'lsq_loss':[], 'reg':[]}
@@ -83,8 +86,8 @@ def TorchLasso(X: torch.tensor, y: torch.tensor, l1: float, opt: torch.optim.Opt
             # forward pass
             y_pred = model.forward(inputs)
             # compute loss
-            lsq = loss(y_pred, targets)
-            pen = model.reg()
+            lsq = loss(y_pred, targets) # =1/N ||Xw-y||^2
+            pen = model.reg() # =2*l1 ||w||_1
             loss_val = lsq + pen            
             # zero gradients
             opt.zero_grad()    

layers.py
@@ -17,10 +17,14 @@ from torch.nn import init
 class L1Linear(torch.nn.Module):
     
 
-    def __init__(self, l1: float, in_features: int, out_features: int, bias: bool = True,
+    def __init__(self, l1: float, in_features: int, out_features: int, bias: bool=True,
                  device=None, dtype=None) -> None:
-        """Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
-           where A is l1-penalized, using the reformulation from
+        """Applies a linear transformation to the incoming data: :math:`y = xW^T + b`
+           where the weights of :math:`W` are l1-penalized, i.e. with the term
+               
+           l1 * ||W||_1
+           
+           The penalty is applied using the reformulation from
             
              Equivalences Between Sparse Models and Neural Networks, Ryan Tibshirani, 2021.
 
@@ -53,7 +57,7 @@ class L1Linear(torch.nn.Module):
                     :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
                     :math:`k = \frac{1}{\text{in\_features}}`
         
-        The weights of the linear transformation A can be retrieved by ``self.weight_u.mul(self.weight_v)``.
+        The weights of the linear transformation :math:`W` can be retrieved by ``self.weight_u.mul(self.weight_v)``.
         
         Examples::
     
@@ -87,9 +91,9 @@ class L1Linear(torch.nn.Module):
     ### Custom methods
     def reg(self):
         """
-        compute l1 * ||W||_1 = l1* (||W_u||^2 + ||W_v||^2)
+        compute l1 * ||W||_1 = (l1/2)* (||W_u||^2 + ||W_v||^2)
         """
-        return self.l1 * (torch.linalg.norm(self.weight_u)**2 + torch.linalg.norm(self.weight_u)**2)
+        return (self.l1/2) * (torch.linalg.norm(self.weight_u)**2 + torch.linalg.norm(self.weight_u)**2)
     
     def get_weight(self):
         return self.weight_u.mul(self.weight_v)