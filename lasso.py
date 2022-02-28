fix scaling

lasso.py
@@ -27,7 +27,7 @@ def TorchLasso(X: torch.tensor, y: torch.tensor, l1: float, opt: torch.optim.Opt
     """
     For :math:`X\\in\\mathbb{R}^{N\\times p}`, solves (if bias=True)
     
-        min_{w,w0}  1/(2*N) * ||Xw + w0 -y||^2 + l1*||w||_1
+        min_{w,w0}  1/N * ||Xw + w0 -y||^2 + l1*||w||_1
         
     Parameters
     ----------
@@ -66,48 +66,52 @@ def TorchLasso(X: torch.tensor, y: torch.tensor, l1: float, opt: torch.optim.Opt
     ds = LassoDataset(X, y)
     dl = DataLoader(ds, batch_size=batch_size, shuffle=False)
     
-    # loss has factor 1/N --> use 2*l1 for penalty
     model = L1Linear(l1=2*l1, in_features=p, out_features=1)
     
-    opt = torch.optim.SGD(model.parameters(), lr=lr)
-        
+    # weight_decay/2 * (||u||^2+||v||^2) 
+    opt = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=l1)
+    
     iterates = list()   
-    info = {'train_loss':[], 'lsq_loss':[], 'reg':[]}
+    info = {'train_loss':[], 'lsq_loss':[], 'reg':[], 'tol':[]}
     
     loss = torch.nn.MSELoss()
     
     for j in torch.arange(n_epochs):
         ################### SETUP FOR EPOCH ##################
-        all_loss = list(); all_lsq = list(); all_reg = list()
+        all_loss = list(); all_lsq = list(); all_reg = list(); all_tol = list()
         ################### START OF EPOCH ###################
         model.train()
         for inputs, targets in dl:
             
+            # zero gradients
+            opt.zero_grad()    
             # forward pass
             y_pred = model.forward(inputs)
             # compute loss
             lsq = loss(y_pred, targets) # =1/N ||Xw-y||^2
-            pen = model.reg() # =2*l1 ||w||_1
-            loss_val = lsq + pen            
-            # zero gradients
-            opt.zero_grad()    
             # backward pass
-            loss_val.backward()    
+            lsq.backward()    
             # iteration
             opt.step()
             
-            all_loss.append(loss_val.item())
+            with torch.no_grad():
+                pen = model.reg() # = l1 ||w||_1
+            
+            all_loss.append(lsq.item()+pen.item())
             all_lsq.append(lsq.item())
             all_reg.append(pen.item())
+            all_tol.append(model.get_tol().item())
             
-            if store_iterates:
-                iterates.append(model.get_weight())
+            
         ################### END OF EPOCH ###################
-        
+        if store_iterates:
+            iterates.append(model.get_weight())
+                
         ### STORE
         info['train_loss'].append(np.mean(all_loss))
         info['lsq_loss'].append(np.mean(all_lsq))
         info['reg'].append(np.mean(all_reg))
+        info['tol'].append(np.mean(all_tol))
         
         if verbose:
             print(f"Epoch {j+1}/{n_epochs}: \t  train loss: {np.mean(all_loss)}.")

testing.py
@@ -4,6 +4,7 @@
 
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.linear_model import lasso_path, Lasso
 
 from layers import L1Linear
@@ -21,9 +22,7 @@ M = L1Linear(l1=0.1, in_features=n, out_features=m, bias=False)
 
 M.weight_u
 
-
 M(X)
-
 
 #%% solve Lasso with Pytorch
 
@@ -41,21 +40,25 @@ X = torch.randn(N,p)
 y = X @ beta + noise*torch.randn(N,1)
 
 #compute Lasso path
-alphas, coef_path, _ = lasso_path(X.numpy(), y.numpy().reshape(-1), alphas=None)
-
 # lasso path uses no intercept!
-#sk = Lasso(alphas[-1], fit_intercept=False).fit(X.numpy(), y.numpy())
-#sk.coef_
+alphas, coef_path, _ = lasso_path(X.numpy(), y.numpy().reshape(-1), alphas=None)
 #coef_path[:,-1]
 
 #%%
 
-l1 = alphas.min() # TorchLasso and scikit have equivalent formulation (i.e. alpha=l1)
+l1 = 2*alphas.min()
+#l1 = 0.5
 batch_size = 10
 
-model, info, iterates = TorchLasso(X, y, l1, bias=False, n_epochs=110, lr=0.01, batch_size=batch_size, store_iterates=True)
+model, info, iterates = TorchLasso(X, y, l1, bias=False, n_epochs=200, lr=0.05, batch_size=batch_size, store_iterates=True)
 
 sol = model.get_weight()
+
+# scikit has factor 1/2N --> scale l1
+sk = Lasso(l1/2, fit_intercept=False).fit(X.numpy(), y.numpy())
+sk.coef_
+
+both_sol = np.vstack((sk.coef_, sol.detach().numpy())).T
 
 #%%
 
@@ -64,6 +67,8 @@ fig, ax = plt.subplots()
 ax.plot(info['train_loss'], label = 'Objective')
 ax.plot(info['lsq_loss'], label = 'Residual loss')
 ax.plot(info['reg'], label = 'Regularization term')
+ax.plot(info['tol'], label = 'max(abs(|u|-|v|))')
+ax.set_yscale('log')
 
 ax.set_xlabel('Epoch')
 ax.legend()
@@ -75,8 +80,8 @@ axs[0].plot(iterates)
 axs[0].set_xlabel('Epoch')
 axs[0].set_title("TorchLasso iterate path")
 
-axs[1].plot(alphas, coef_path.T, '-o', markersize = 0.8)
+axs[1].plot(2*alphas, coef_path.T, '-o', markersize = 0.8)
 axs[1].set_xscale('log')
-axs[1].set_xlabel('log(Lambda)')
+axs[1].set_xlabel('log(l1)')
 axs[1].set_title("Lasso path")
 axs[1].set_ylim(axs[0].get_ylim())